2dfe2f39ab9464a791fd42b276fcb321917ba241
Bryce Lelbach <admin@thefireflyproject.us>
2010-10-12 22:32:21 -0400

    Turned fast_string into a template to increase modularity and reusability and decrease internal dependencies (previously it required a complete definition of list).

--- boost/spirit/home/prana/strings/fast_string.hpp
+++ boost/spirit/home/prana/strings/fast_string.hpp
@@ -10,49 +10,82 @@
 #if !defined(BOOST_SPIRIT_PRANA_FAST_STRING_HPP)
 #define BOOST_SPIRIT_PRANA_FAST_STRING_HPP
 
-#include <boost/type_traits/alignment_of.hpp>
+#include <boost/swap.hpp>
 
-#include <boost/spirit/home/prana/adt/list_fwd.hpp>
+#include <boost/type_traits/alignment_of.hpp>
 
 namespace boost {
 namespace spirit {
 namespace prana {
 
-// keep as POD! do not implement dtor
-struct fast_string {
+/*=============================================================================
+fast_string<> is a primitive string implementation designed for internal use in
+utree and linked_list<>. Originally, fast_string<> was not a template; it's
+buffer size was linked to linked_list<> (which was also originally not a
+template). I (wash) templatized fast_string<> during my effort to reduce
+dependencies between utree utilities, thus making utree more modular.
+
+fast_string<> is deliberately a POD to allow it to be placed in a union. This
+structure utilizes (sizeof(List) * alignment_of(List)) / sizeof(char) In a 32
+bit system, this is 14 bytes. The two extra bytes are used by utree to store
+management info.
+
+This is a const string (i.e. immutable). It stores the characters directly if
+possible and only uses the heap if the string does not fit. Null characters are
+allowed, making it suitable to encode raw binary. The string length is encoded
+in the first byte if the string is placed in-situ, else, the length plus a
+pointer to the string in the heap are stored.
+=============================================================================*/
+
+template<typename List>
+class fast_string {
  public:
-  static std::size_t const buff_size =
-    (sizeof(list) + boost::alignment_of<list>::value) / sizeof(char);
+  typedef std::size_t size_type;
+  
+  BOOST_STATIC_CONSTANT(size_type, buff_size =
+    (sizeof(List) + boost::alignment_of<List>::value) / sizeof(char));
 
-  static std::size_t const small_string_size =
-    buff_size - sizeof(char);
+  BOOST_STATIC_CONSTANT(size_type, small_string_size =
+    buff_size - sizeof(char));
 
-  static std::size_t const max_string_len =
-    small_string_size - 1;
+  BOOST_STATIC_CONSTANT(size_type, max_string_len =
+    small_string_size - 1);
 
   struct heap_store {
     char* str;
     std::size_t size;
   };
 
+ private:
   union {
     char buff[buff_size];
     heap_store heap;
   };
 
-  int get_type (void) const { return info() >> 1; }
-  void set_type (int t) { info() = (t << 1) | (info() & 1); }
-  bool is_heap_allocated (void) const { return info() & 1; } 
+ public:
+  #define INFO buff[small_string_size]
 
-  std::size_t size (void) const {
-    if (is_heap_allocated())
+  int get_type (void) const {
+    return INFO >> 1;
+  }
+  
+  void set_type (int t) {
+    INFO = (t << 1) | (INFO & 1);
+  }
+
+  bool is_heap_allocated (void) const {
+    return INFO & 1;
+  } 
+
+  size_type size (void) const {
+    if (INFO & 1) // aka is_heap_allocated()
       return heap.size;
     else
       return max_string_len - buff[small_string_size - 1];
   }
 
   char const* str (void) const {
-    if (is_heap_allocated())
+    if (INFO & 1) // aka is_heap_allocated()
       return heap.str;
     else
       return buff;
@@ -60,34 +93,36 @@ struct fast_string {
 
   template<typename Iterator>
   void construct (Iterator f, Iterator l) {
-    unsigned const size = l - f;
+    size_type size = l - f;
     char* str = 0;
 
     if (size < small_string_size) {
-    // if it fits, store it in-situ; small_string_size minus the length
-    // of the string is placed in buff[small_string_size - 1]
+      // if it fits, store it in-situ; small_string_size - length of the string
+      // is placed in buff[small_string_size - 1]
       str = buff;
       buff[small_string_size - 1] = static_cast<char>(max_string_len - size);
-      info() &= ~0x1;
+      INFO &= ~0x1;
     }
 
-    else {
-      // else, store it in the heap
+    else { // else, store it in the heap
       str = new char[size + 1]; // add one for the null char
       heap.str = str;
       heap.size = size;
-      info() |= 0x1;
+      INFO |= 0x1;
     }
 
+    // FIXME: there's gotta be some way to unroll this with TMP
     for (std::size_t i = 0; i != size; ++i) *str++ = *f++;
 
     *str = '\0'; // add the null char
   }
 
-  void swap (fast_string& other) { std::swap(*this, other); }
+  void swap (fast_string& other) {
+    boost::swap(*this, other);
+  }
   
   void free (void) { 
-    if (heap.str && is_heap_allocated()) {
+    if (heap.str && (INFO & 1)) {
       delete[] heap.str;
       heap.str = 0;
     } 
@@ -97,8 +132,13 @@ struct fast_string {
     construct(other.str(), other.str() + other.size());
   }
 
-  char& info (void) { return buff[small_string_size]; }
-  char info (void) const { return buff[small_string_size]; }
+  char& info (void) {
+    return buff[small_string_size];
+  }
+  
+  char info (void) const {
+    return buff[small_string_size];
+  }
 
   short tag (void) const { 
     return (int(buff[small_string_size - 2]) << 8) + buff[small_string_size - 1];
