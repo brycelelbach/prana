5d6ffd875dba9f9d07e73e5cac055cda2829e9f3
Bryce Lelbach <admin@thefireflyproject.us>
2010-11-03 08:58:18 -0400

    Added FIXME notes to utree, as well as a dummy clear method.

--- boost/spirit/home/prana/utree.hpp
+++ boost/spirit/home/prana/utree.hpp
@@ -74,6 +74,8 @@ class utree {
 
   boost::uint8_t kind (void) const;
 
+  void clear (void);
+
  private:
   boost::uint8_t _kind;
   union {
@@ -93,52 +95,72 @@ utree<Policy>::utree (void) {
 
 template<typename Policy>
 utree<Policy>::utree (utree const& other) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(other);
 }
 
 template<typename Policy>
 template<typename Copier>
 utree<Policy>::utree (utree const& other, Copier copier) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(other, copier);
 }
 
 template<typename Policy>
 utree<Policy>::utree (bool bool_) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(bool_);
 }
 
 template<typename Policy>
 utree<Policy>::utree (char char_) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(char_);
 }
 
 template<typename Policy>
 utree<Policy>::utree (int int_) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(int_);
 }
 
 template<typename Policy>
 utree<Policy>::utree (double double_) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(double_);
 }
 
 template<typename Policy>
 utree<Policy>::utree (char const* str) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(str);
 }
 
 template<typename Policy>
 utree<Policy>::utree (char const* first, char const* last) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(first, last);
 }
 
 template<typename Policy>
 utree<Policy>::utree (char const* bits, size_type len) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(bits, len);
 }
 
 template<typename Policy>
 inline utree<Policy>::utree (std::string const& str) {
+  // FIXME: Inline copy by hand here to ensure that we avoid pointless
+  // equality-checking and clearing.
   copy(str);
 }
 
@@ -185,17 +207,26 @@ inline utree<Policy>& utree<Policy>::copy (utree const& other) {
 template<typename Policy>
 template<typename Copier>
 utree<Policy>& utree<Policy>::copy (utree const& other, Copier copier) {
+  // FIXME: Check if this is a reference; if it is, check if the reference is
+  // equal to the source, and return early if it is.
+  clear();
   copier(other, other._kind, *this, _kind);
 }
 
 template<typename Policy>
 utree<Policy>& utree<Policy>::copy (bool bool_) {
+  // FIXME: Check if this is a bool; if it is, check if the bool is
+  // equal to the source, and return early if it is.
+  clear();
   _bool = bool_;
   _kind = bool_kind; 
 }
 
 template<typename Policy>
 utree<Policy>& utree<Policy>::copy (char char_) {
+  // FIXME: Check if this is a symbol; if it is, check if the symbol is
+  // equal to the source, and return early if it is.
+  clear();
   _symbol.default_construct();
   _symbol.deep_copy((char const*) &char_, (char const*) 0); 
   _kind = symbol_kind; 
@@ -203,18 +234,27 @@ utree<Policy>& utree<Policy>::copy (char char_) {
 
 template<typename Policy>
 utree<Policy>& utree<Policy>::copy (int int_) {
+  // FIXME: Check if this is a integer; if it is, check if the integer is
+  // equal to the source, and return early if it is.
+  clear();
   _integer = int_;
   _kind = integer_kind;
 }
 
 template<typename Policy>
 utree<Policy>& utree<Policy>::copy (double double_) {
+  // FIXME: Check if this is a double; if it is, check if the double is
+  // equal to the source, and return early if it is.
+  clear();
   _floating = double_;
   _kind = floating_kind; 
 }
 
 template<typename Policy>
 utree<Policy>& utree<Policy>::copy (char const* str) {
+  // FIXME: Check if this is a symbol; if it is, check if the symbol is
+  // equal to the source, and return early if it is.
+  clear();
   _symbol.default_construct();
   _symbol.deep_copy(str);
   _kind = symbol_kind; 
@@ -222,6 +262,9 @@ utree<Policy>& utree<Policy>::copy (char const* str) {
 
 template<typename Policy>
 utree<Policy>& utree<Policy>::copy (char const* first, char const* last) {
+  // FIXME: Check if this is a symbol; if it is, check if the symbol is
+  // equal to the source, and return early if it is.
+  clear();
   _symbol.default_construct();
   _symbol.deep_copy(first, last);
   _kind = symbol_kind; 
@@ -229,22 +272,34 @@ utree<Policy>& utree<Policy>::copy (char const* first, char const* last) {
 
 template<typename Policy>
 utree<Policy>& utree<Policy>::copy (char const* bits, size_type len) {
+  // FIXME: Check if this is a symbol; if it is, check if the symbol is
+  // equal to the source, and return early if it is.
+  clear();
   _symbol.default_construct();
   _symbol.deep_copy(bits, bits + len);
-  _kind = symbol_kind; 
+  _kind = symbol_kind;
 }
 
 template<typename Policy>
 utree<Policy>& utree<Policy>::copy (std::string const& str) {
+  // FIXME: Check if this is a symbol; if it is, check if the symbol is
+  // equal to the source, and return early if it is.
+  clear();
   _symbol.default_construct();
   _symbol.deep_copy(str);
-  _kind = symbol_kind; 
+  _kind = symbol_kind;
 }
 
 template<typename Policy>
 boost::uint8_t utree<Policy>::kind (void) const {
   return _kind | ~reference_kind;
 }
+
+template<typename Policy>
+void utree<Policy>::clear (void) {
+  // FIXME: implement clear semantics
+}
+
 
 } // prana
 } // spirit
