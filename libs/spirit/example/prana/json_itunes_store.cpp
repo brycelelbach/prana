f8de5a383e2b71bfc08a86f6499a557122f37dd3
Bryce Lelbach <admin@thefireflyproject.us>
2011-01-05 10:04:48 -0500

    Added comments explaining the iTunes store example.

--- libs/spirit/example/prana/json_itunes_store.cpp
+++ libs/spirit/example/prana/json_itunes_store.cpp
@@ -1,5 +1,5 @@
 /*==============================================================================
-    Copyright (c) 2010      Bryce Lelbach
+    Copyright (c) 2011      Bryce Lelbach
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -20,16 +20,31 @@ int main (void) {
 
   std::ifstream ifs("data/itunes_store.json");
 
+  // Ignore the windows UTF8 bom, if it is there.
   ignore_utf8_bom(ifs, "data/itunes_store.json"); 
 
+  // utree is the low-level, generic AST used in Prana.
   utree ast;
 
+  // This is the low-level interface. It takes an input form (a string, stream,
+  // or utree holding a string currently) as it's first parameter, and
+  // produces an output, storing it in it's second parameter. 
   if (!parse_json(ifs, ast))
     throw parse_exception();
-    
+
+  // json_ir is a DSIR - a domain specific intermediate representation. It's a
+  // shallow view into the underlying utree AST. 
   json_ir ir(ast);
  
   std::cout << ir["results"][0]["artworkUrl100"] << std::endl;
+  //           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // This is actually a proto expression. It will never throw; if the specified
+  // JSON node doesn't exist, a sentinel node is returned. The underlying utree
+  // type of the sentinel node is invalid_type. Note that the type of the above
+  // expression is a Proto expression wrapper, which happens to be streamable.
+  // It is additionally convertible to utree and json_ir, and it also provides
+  // a get member that will explicitly retrieve the underlying utree node. The
+  // output from this example should be: 
   // "http://a1.phobos.apple.com/us/r1000/056/Purple/e2/2b/7a/mzi.opitdvsr.tif"
 }
  
