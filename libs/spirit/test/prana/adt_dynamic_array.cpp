aa2c67e7f06ceaa862f38aed7d01bbfd1d915f71
wash <wash@vega.cct.lsu.edu>
2011-04-02 21:38:07 -0500

    Killed signedness warnings.

--- libs/spirit/test/prana/adt_dynamic_array.cpp
+++ libs/spirit/test/prana/adt_dynamic_array.cpp
@@ -21,18 +21,18 @@ int main (void) { try {
     t.push_back(1);
     t.push_back(2);
     t.push_back(3);
-    BOOST_TEST_EQ(t.size(), 3);
+    BOOST_TEST_EQ(t.size(), array_type::size_type(3));
 
     // lookup
-    BOOST_TEST_EQ(t[0], 1);
-    BOOST_TEST_EQ(t[1], 2);
-    BOOST_TEST_EQ(t[2], 3);
+    BOOST_TEST_EQ(t[0], unsigned(1));
+    BOOST_TEST_EQ(t[1], unsigned(2));
+    BOOST_TEST_EQ(t[2], unsigned(3));
 
     // removal
     t.pop_back();
     t.pop_back();
     t.pop_back();
-    BOOST_TEST_EQ(t.size(), 0);
+    BOOST_TEST_EQ(t.size(), array_type::size_type(0));
   }
   
   { 
@@ -40,18 +40,18 @@ int main (void) { try {
 
     // insertion
     for (unsigned i = 0; i < 8; ++i) { t.push_back(i + 1); }
-    BOOST_TEST_EQ(t.size(), 8);
-    BOOST_TEST_EQ(t.capacity(), 8);
+    BOOST_TEST_EQ(t.size(), array_type::size_type(8));
+    BOOST_TEST_EQ(t.capacity(), array_type::size_type(8));
     t.push_back(9);
-    BOOST_TEST_EQ(t.size(), 9);
-    BOOST_TEST_EQ(t.capacity(), 16);
+    BOOST_TEST_EQ(t.size(), array_type::size_type(9));
+    BOOST_TEST_EQ(t.capacity(), array_type::size_type(16));
 
     // lookup
     for (unsigned i = 0; i < 9; ++i) { BOOST_TEST_EQ(t[i], (i + 1)); }
 
     // removal
     t.clear();
-    BOOST_TEST_EQ(t.size(), 0);
+    BOOST_TEST_EQ(t.size(), array_type::size_type(0));
   }
 
   { 
@@ -61,30 +61,30 @@ int main (void) { try {
     t0.push_back(1);
     t0.push_back(2);
     t0.push_back(3);
-    BOOST_TEST_EQ(t0.size(), 3);
-    BOOST_TEST_EQ(t1.size(), 0);
+    BOOST_TEST_EQ(t0.size(), array_type::size_type(3));
+    BOOST_TEST_EQ(t1.size(), array_type::size_type(0));
 
     // copy
     t1 = t0;
-    BOOST_TEST_EQ(t0.size(), 3);
-    BOOST_TEST_EQ(t1.size(), 3);
+    BOOST_TEST_EQ(t0.size(), array_type::size_type(3));
+    BOOST_TEST_EQ(t1.size(), array_type::size_type(3));
 
     // clear first
     t0.clear();
-    BOOST_TEST_EQ(t0.size(), 0);
-    BOOST_TEST_EQ(t1.size(), 3);
+    BOOST_TEST_EQ(t0.size(), array_type::size_type(0));
+    BOOST_TEST_EQ(t1.size(), array_type::size_type(3));
 
     // lookup
-    BOOST_TEST_EQ(t1[0], 1);
-    BOOST_TEST_EQ(t1[1], 2);
-    BOOST_TEST_EQ(t1[2], 3);
+    BOOST_TEST_EQ(t1[0], unsigned(1));
+    BOOST_TEST_EQ(t1[1], unsigned(2));
+    BOOST_TEST_EQ(t1[2], unsigned(3));
 
     // removal
     t1.pop_back();
     t1.pop_back();
     t1.pop_back();
-    BOOST_TEST_EQ(t0.size(), 0);
-    BOOST_TEST_EQ(t1.size(), 0);
+    BOOST_TEST_EQ(t0.size(), array_type::size_type(0));
+    BOOST_TEST_EQ(t1.size(), array_type::size_type(0));
   }
   
   } catch (std::exception& e) {
