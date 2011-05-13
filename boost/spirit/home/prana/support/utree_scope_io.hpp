ee3a935ab85429c0a01ad34953b768debd738e34
Bryce Lelbach (wash) <blelbach@cct.lsu.edu>
2011-05-13 07:52:09 -0500

    Add streaming operator for spirit::scope to ease debugging.

---
+++ boost/spirit/home/prana/support/utree_scope_io.hpp
@@ -0,0 +1,35 @@
+/*==============================================================================
+    Copyright (c) 2010      Bryce Lelbach
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#if !defined(BSP_9835A676_570F_40DE_A3B3_C62EEB1DCF36)
+#define BSP_9835A676_570F_40DE_A3B3_C62EEB1DCF36
+
+#include <ostream>
+
+#include <boost/spirit/include/support_utree.hpp>
+
+namespace boost {
+namespace spirit {
+
+std::ostream& operator<< (std::ostream& out, scope const& env) {
+  out << "level[" << env.level() << "]";
+
+  BOOST_FOREACH(utree const& e, env) {
+    out << "  " << e;
+  }
+
+  if (env.outer())
+    out << env.outer();
+
+  return out;
+}
+
+} // spirit
+} // boost
+
+#endif // BSP_9835A676_570F_40DE_A3B3_C62EEB1DCF36
+
