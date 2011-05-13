7b6eada33d7bb94364eac4b96b56925763c9bf29
Bryce Lelbach (wash) <blelbach@cct.lsu.edu>
2011-05-13 07:56:38 -0500

    Use placeholder class to determine when we're handling the expansion of a first class function object.

--- libs/spirit/src/phxpr/evaluator.cpp
+++ libs/spirit/src/phxpr/evaluator.cpp
@@ -5,6 +5,7 @@
 //  file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <typeinfo>
 #include <boost/assert.hpp>
 #include <boost/fusion/include/at_c.hpp>
 
@@ -64,7 +65,7 @@ struct evaluator_visitor {
 
   template<class T>
   result_type operator() (T const& val) const {
-    return result_type(utree(val));
+    return result_type(utree(val)); 
   }
 
   result_type operator() (utf8_symbol_range_type const& str) const {
@@ -85,12 +86,19 @@ struct evaluator_visitor {
     > func = (*variables)[sym];
 
     if (at_c<1>(func)) {
-      if (at_c<0>(func)->second.target<procedure>())
-        return at_c<0>(func)->second.target<procedure>()->body->f; 
+      if (at_c<0>(func)->second.target<placeholder>()) {
+        placeholder const* p = at_c<0>(func)->second.target<placeholder>();
+        return function(p->body->f, p->body->arity, p->body->fixed, p->level);
+      }
+
+      else if (at_c<0>(func)->second.target<procedure>()) {
+        procedure const* p = at_c<0>(func)->second.target<procedure>();
+        return quote(*p->body);
+      }
 
       // TODO: eliminate this case, by storing all compiled functions in
       // procedure instances.
-      return (at_c<0>(func)->second)(actor_list());
+      return (at_c<0>(func)->second)(boost::shared_ptr<actor_list>());
     } // }}}
     
     // TODO: replace with exception (identifier not found error)
@@ -99,105 +107,7 @@ struct evaluator_visitor {
   } // }}}
 
   template<class Iterator>
-  result_type operator() (iterator_range<Iterator> const& range) const {
-    // {{{ nary variable/macro implementation
-    using boost::fusion::at_c;
-
-    typename iterator_range<Iterator>::const_iterator it = range.begin(),
-                                                      end = range.end();
-
-    // TODO: replace with exception
-    BOOST_ASSERT(it != end);
-
-    // TODO: consider moving this to a separate method
-    // {{{ anonymous lambda or indirect procedure call 
-    if (is_utree_container(*it)) {
-      boost::shared_ptr<function> anon(new function);
-      *anon = utree::visit(*it, *this);
-      
-      ++it;
-
-      utree use(iterator_range<Iterator>(it, range.end()), spirit::shallow);
-
-      boost::shared_ptr<actor_list> flist(new actor_list);
-
-      BOOST_FOREACH(utree const& e, use) {
-        flist->push_back(utree::visit(e, *this));
-      }
-
-      return lambda_function(flist, anon, variables->level()); 
-    } // }}}
-
-    std::string sym = get_symbol(*it);
-    ++it;
-    
-    // nullary call
-    if (it == end) {
-      // TODO: replace with exceptions
-      BOOST_ASSERT(sym != "variable");
-      BOOST_ASSERT(sym != "macro");
-      BOOST_ASSERT(sym != "quote");
-      BOOST_ASSERT(sym != "lambda");
-    }
-
-    if (sym == "variable") {
-      utree body(iterator_range<Iterator>(it, range.end()), spirit::shallow);
-      return define_variable(body);
-    }
-
-    if (sym == "macro") {
-      utree body(iterator_range<Iterator>(it, range.end()), spirit::shallow);
-      return define_macro(body);
-    }
-
-    if (sym == "quote")
-      return quote(*it);
-
-    if (sym == "lambda") {
-      utree body(iterator_range<Iterator>(it, range.end()), spirit::shallow);
-      return make_lambda(body);
-    }
-
-    { // {{{ macro expansion
-      fusion::vector3<
-        environment<macro>::const_iterator, bool, scope::size_type
-      > macro = (*macros)[sym];
-
-      if (at_c<1>(macro)) {
-        utree use(iterator_range<Iterator>(it, range.end()), spirit::shallow);
-
-        boost::shared_ptr<matcher> match = at_c<0>(macro)->second.match(use);
-
-        if (match) {
-          boost::shared_ptr<utree> expansion = match->expand();
-          // TODO: make this an exception
-          BOOST_ASSERT(expansion);
-          return utree::visit(*expansion, *this);
-        }
-      }
-    } // }}}
-
-    // {{{ function creation
-    fusion::vector3<
-      environment<compiled_function>::const_iterator, bool, scope::size_type
-    > func = (*variables)[sym];
-
-    if (at_c<1>(func)) {
-      utree use(iterator_range<Iterator>(it, range.end()), spirit::shallow);
-
-      actor_list flist;
-
-      BOOST_FOREACH(utree const& e, use) {
-        flist.push_back(utree::visit(e, *this));
-      }
-
-      return (at_c<0>(func)->second)(flist);
-    } // }}}
- 
-    // TODO: replace with exception (identifier not found error)
-    BOOST_ASSERT(false);
-    return result_type();
-  } // }}}
+  result_type operator() (iterator_range<Iterator> const& range) const;
 
   result_type operator() (any_ptr const& p) const {
     // TODO: make this an exception
@@ -237,8 +147,10 @@ struct evaluator_visitor {
     function r = (*this)(*it);
     
     func->f = r.f;
+    func->arity = r.arity; 
     func->fixed = r.fixed;
-    
+    func->level = r.level;
+ 
     return result_type();  
   }
   
@@ -272,12 +184,163 @@ evaluator::~evaluator (void) {
     BOOST_FOREACH(environment<compiled_function>::value_type const& e,
                   variables.definitions)
     {
-      if (e.second.target<procedure>()) {
+      if (e.second.target<procedure>())
         e.second.target<procedure>()->body->f.clear();
-      }
+      else if (e.second.target<placeholder>())
+        e.second.target<placeholder>()->body->f.clear();
     } 
   }
 }
+
+struct trampoline_function: actor<trampoline_function> {
+  boost::shared_ptr<placeholder> ph;
+  boost::shared_ptr<actor_list> elements;
+
+  typedef utree result_type;
+
+  trampoline_function (placeholder const& ph_,
+                       boost::shared_ptr<actor_list> const& elements_):
+    ph(new placeholder(ph_.body, ph_.level)), elements(elements_)
+  {
+    BOOST_ASSERT(ph);
+    BOOST_ASSERT(elements);
+  }
+  
+  utree eval (scope const& env) const {
+    scope const* outer = &env; // Get the parent scope.
+
+    while (ph->level != outer->level())
+      outer = outer->outer();
+      
+    if (elements && !elements->empty()) {
+      boost::scoped_array<utree> fargs(new utree[elements->size()]);
+
+      std::size_t i = 0;
+
+      BOOST_FOREACH(function const& element, *elements) {
+        fargs[i++] = element(env);
+      }
+
+      utree* fi = fargs.get();
+
+      utree real_f = ph->body->eval(env);
+      return real_f.eval(scope(fi, fi + elements->size()));
+    }
+    
+    else {
+      utree real_f = ph->body->eval(env);
+      return real_f.eval(scope(0, 0));
+    }
+  }
+};
+  
+template<class Iterator>
+evaluator_visitor::result_type
+evaluator_visitor::operator() (iterator_range<Iterator> const& range) const {
+  // {{{ nary variable/macro implementation
+  using boost::fusion::at_c;
+
+  typename iterator_range<Iterator>::const_iterator it = range.begin(),
+                                                    end = range.end();
+
+  // TODO: replace with exception
+  BOOST_ASSERT(it != end);
+
+  // TODO: consider moving this to a separate method
+  // {{{ anonymous lambda or indirect procedure call 
+  if (is_utree_container(*it)) {
+    boost::shared_ptr<function> anon(new function);
+    *anon = utree::visit(*it, *this);
+    
+    ++it;
+
+    utree use(iterator_range<Iterator>(it, range.end()), spirit::shallow);
+
+    boost::shared_ptr<actor_list> flist(new actor_list);
+
+    BOOST_FOREACH(utree const& e, use) {
+      flist->push_back(utree::visit(e, *this));
+    }
+
+    return lambda_function(flist, anon, variables->level()); 
+  } // }}}
+
+  std::string sym = get_symbol(*it);
+  ++it;
+  
+  // nullary call
+  if (it == end) {
+    // TODO: replace with exceptions
+    BOOST_ASSERT(sym != "variable");
+    BOOST_ASSERT(sym != "macro");
+    BOOST_ASSERT(sym != "quote");
+    BOOST_ASSERT(sym != "lambda");
+  }
+
+  if (sym == "variable") {
+    utree body(iterator_range<Iterator>(it, range.end()), spirit::shallow);
+    return define_variable(body);
+  }
+
+  if (sym == "macro") {
+    utree body(iterator_range<Iterator>(it, range.end()), spirit::shallow);
+    return define_macro(body);
+  }
+
+  if (sym == "quote")
+    return quote(*it);
+
+  if (sym == "lambda") {
+    utree body(iterator_range<Iterator>(it, range.end()), spirit::shallow);
+    return make_lambda(body);
+  }
+
+  { // {{{ macro expansion
+    fusion::vector3<
+      environment<macro>::const_iterator, bool, scope::size_type
+    > macro = (*macros)[sym];
+
+    if (at_c<1>(macro)) {
+      utree use(iterator_range<Iterator>(it, range.end()), spirit::shallow);
+
+      boost::shared_ptr<matcher> match = at_c<0>(macro)->second.match(use);
+
+      if (match) {
+        boost::shared_ptr<utree> expansion = match->expand();
+        // TODO: make this an exception
+        BOOST_ASSERT(expansion);
+        return utree::visit(*expansion, *this);
+      }
+    }
+  } // }}}
+
+  // {{{ function creation
+  fusion::vector3<
+    environment<compiled_function>::const_iterator, bool, scope::size_type
+  > func = (*variables)[sym];
+
+  if (at_c<1>(func)) {
+    utree use(iterator_range<Iterator>(it, range.end()), spirit::shallow);
+
+    boost::shared_ptr<actor_list> flist(new actor_list);
+    
+    BOOST_FOREACH(utree const& e, use) {
+      flist->push_back(utree::visit(e, *this));
+    }
+
+    // handle placeholder-as-procedure
+    if (at_c<0>(func)->second.target<placeholder>()) {
+      return trampoline_function
+        (*at_c<0>(func)->second.target<placeholder>(), flist);
+    }
+
+    return (at_c<0>(func)->second)(flist);
+  } // }}}
+
+  // TODO: replace with exception (identifier not found error)
+  BOOST_ASSERT(false);
+  return result_type();
+} // }}}
 
 // forwarders
 evaluator::result_type evaluator::operator() (utree const& ut) {
@@ -296,15 +359,15 @@ evaluator_visitor::make_lambda (utree const& ut) const {
   for (utree::size_type i = 0; i < ut.front().size(); ++i, ++it) {
     if (!fixed_arity && ((ut.front().size() - 1) == i)) {
       local.variables.define(get_symbol(*it),
-        procedure(boost::shared_ptr<function>
-          (new function(phxpr::varg(i, local.variables.level()).f, false)),
+        placeholder(boost::shared_ptr<function>
+          (new function(phxpr::varg(i, local.variables.level()))),
            local.variables.level()));
       break;
     }
 
     local.variables.define(get_symbol(*it),
-      procedure(boost::shared_ptr<function>
-        (new function(phxpr::arg(i, local.variables.level()).f)),
+      placeholder(boost::shared_ptr<function>
+        (new function(phxpr::arg(i, local.variables.level()))),
          local.variables.level()));
   }
 
@@ -321,9 +384,11 @@ evaluator_visitor::make_lambda (utree const& ut) const {
 
   // REVIEW: pass fixed_arity to the function instance that's returned?
   if (flist->size() > 1)
-    return begin(flist);
+    return function
+      (begin(flist).f, ut.front().size(), fixed_arity, local.variables.level());
   else
-    return flist->front();   
+    return function
+      (flist->front().f, ut.front().size(), fixed_arity, local.variables.level());
 }
 
 } // phxpr
