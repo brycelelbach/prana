cf2b66272ad184183199935127bbf22b36145887
Bryce Lelbach (wash) <blelbach@cct.lsu.edu>
2011-06-12 15:50:43 -0500

    Add a recursive reference actor.

--- phxpr/primitives/reference.hpp
+++ phxpr/primitives/reference.hpp
@@ -32,7 +32,14 @@ struct variable_reference: actor<variable_reference> {
                       phxpr::shared_ptr<gpt_type> const& gpt):
     value(value_), global_procedure_table(gpt)
   { }
-  
+
+/*  
+  ~variable_reference (void) {
+    if (value.unique())
+      value->clear();
+  }
+*/
+
   // TODO: Refactor this code with the code in procedure
   utree eval (utree& ut) const { 
     BOOST_ASSERT(value);
@@ -64,6 +71,50 @@ struct variable_reference: actor<variable_reference> {
   // assignment once it's implemented?
   function_base* copy (void) const
   { return new variable_reference(value, global_procedure_table); }
+};
+
+struct recursive_reference: actor<recursive_reference> {
+  typedef sheol::adt::dynamic_array<signature> gpt_type;
+
+  phxpr::shared_ptr<utree> const& value;
+  phxpr::shared_ptr<gpt_type> global_procedure_table;
+ 
+  recursive_reference (phxpr::shared_ptr<utree> const& value_,
+                       phxpr::shared_ptr<gpt_type> const& gpt):
+    value(value_), global_procedure_table(gpt)
+  { }
+
+  // TODO: Refactor this code with the code in procedure
+  utree eval (utree& ut) const { 
+    BOOST_ASSERT(value);
+
+    using boost::fusion::at_c;
+   
+    runtime_environment& env = *ut.get<runtime_environment*>();
+
+    if (prana::recursive_which(*value) == utree_type::function_type) {
+      BOOST_ASSERT(value->tag() <= global_procedure_table->size());
+
+      // Load the argument's signature from the gpt.
+      signature const& sig = (*global_procedure_table)[value->tag()];
+
+//      std::cout << at_c<3>(sig) << std::endl;
+
+#if 0
+      if ((at_c<3>(sig) == function_type::placeholder)||
+          (at_c<3>(sig) == function_type::thunk))
+        return utree(new recursive_reference
+          (env.invoke(value), global_procedure_table));
+#endif
+    }
+
+    return *value;
+  }
+
+  // REVIEW: Will passing shallow references via smart pointers screw with
+  // assignment once it's implemented?
+  function_base* copy (void) const
+  { return new recursive_reference(value, global_procedure_table); }
 };
 
 } // phxpr
